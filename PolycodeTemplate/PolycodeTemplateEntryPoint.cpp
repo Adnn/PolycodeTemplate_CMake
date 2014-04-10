da0610f0d78466bf34e28f508fa6f574c4ad1056
Adrien <adrien@astutegraphics.co.uk>
2014-04-10 17:57:11 +0200

    Minor. Changes the .cpp file to Allman style.

--- PolycodeTemplate/PolycodeTemplateEntryPoint.cpp
+++ PolycodeTemplate/PolycodeTemplateEntryPoint.cpp
@@ -4,7 +4,8 @@
 
 #include "PolycodeTemplateEntryPoint.h"
 
-PolycodeTemplateEntryPoint::PolycodeTemplateEntryPoint(PolycodeView *view) {
+PolycodeTemplateEntryPoint::PolycodeTemplateEntryPoint(PolycodeView *view)
+{
     core = new POLYCODE_CORE(view, 640,480,false,false, 0,0,60);
     Polycode::CoreServices::getInstance()->getResourceManager()->addArchive("default.pak");
     Polycode::CoreServices::getInstance()->getResourceManager()->addDirResource("default");
@@ -12,10 +13,12 @@ PolycodeTemplateEntryPoint::PolycodeTemplateEntryPoint(PolycodeView *view) {
 	// Write your code here!
 }
 
-PolycodeTemplateEntryPoint::~PolycodeTemplateEntryPoint() {
+PolycodeTemplateEntryPoint::~PolycodeTemplateEntryPoint()
+{
     
 }
 
-bool PolycodeTemplateEntryPoint::Update() {
+bool PolycodeTemplateEntryPoint::Update()
+{
     return core->updateAndRender();
 }
